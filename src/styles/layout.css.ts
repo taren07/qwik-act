92361bf89c6ce6ec26c809c933832238205bc135
taren07 <taro.enchi@gmail.com>
2024-11-23 10:55:06 +0900

    add vanilla-extract css

---
+++ src/styles/layout.css.ts
@@ -0,0 +1,8 @@
+import { style } from "@vanilla-extract/css";
+
+export const layoutStyle = style({
+	display: "block",
+	width: "100%",
+	height: "500px",
+	background: "white",
+});
--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,8 @@ import { qwikVite } from "@builder.io/qwik/optimizer";
 import { qwikCity } from "@builder.io/qwik-city/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import pkg from "./package.json";
-import { vanillaExtractPlugin } from "styled-vanilla-extract/vite";
+// import { vanillaExtractPlugin } from "styled-vanilla-extract/vite";
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 type PkgDep = Record<string, string>;
 const { dependencies = {}, devDependencies = {} } = pkg as any as {
   dependencies: PkgDep;
